ca31e22daf847c790794f5b2d361ced1c465f540
Bukhari bin mahmoud Kibuka <93518580+bumahkib7@users.noreply.github.com>
2026-03-28 08:59:27 +0000

    Update runix to v0.1.0 with SHA256 hashes

--- Formula/runix.rb
+++ Formula/runix.rb
@@ -7,24 +7,24 @@ class Runix < Formula
   on_macos do
     on_arm do
       url "https://github.com/bumahkib7/runix/releases/download/v#{version}/runix-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "6dbe50050263806cb2b328e59a2017a63c68e04e2c2e33b3522e9af714108bf9"
     end
 
     on_intel do
       url "https://github.com/bumahkib7/runix/releases/download/v#{version}/runix-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "36a0c4c42e960597293173b98b26821c2a2cbe7ac4818f250c7d48b3b75e018f"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/bumahkib7/runix/releases/download/v#{version}/runix-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "e4da0920bd4f3f0cfc42a461b25a269fa06484c3d939063f536f07ad61d1794d"
     end
 
     on_intel do
       url "https://github.com/bumahkib7/runix/releases/download/v#{version}/runix-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "e8f6b8cd12566ae5ea2dd0409368f240285870138097420394050b09d80796f4"
     end
   end
 
